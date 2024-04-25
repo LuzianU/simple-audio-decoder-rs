3799a8830eb776ac85390d394170d15a78c21894
LuzianU <52568586+LuzianU@users.noreply.github.com>
2024-04-25 16:27:28 +0200

    update header

--- src/simple_audio_decoder_rs.h
+++ src/simple_audio_decoder_rs.h
@@ -17,7 +17,7 @@ void *pcm_new_from_raw(const void *data, size_t size, size_t sample_rate, size_t
 
 void pcm_free(void *pcm_ptr);
 
-void *audio_clip_new(const char *file, size_t target_sample_rate, size_t chunk_size);
+void *audio_clip_new(const void *pcm_pointer, size_t target_sample_rate, size_t chunk_size);
 
 void audio_clip_free(void *audio_clip_ptr);
 
